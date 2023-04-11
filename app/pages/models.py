1fec73a518651dde5c1c4123e1b59225d75dc4ab
Nico <nicolo.forti@code.berlin>
2023-04-11 18:33:55 +0200

    added lates models db

--- app/pages/models.py
+++ app/pages/models.py
@@ -5,8 +5,8 @@ class Project(db.Model, CRUDMixin):
     slug = db.Column(db.String(80), unique=True)
     name = db.Column(db.String(80))
     picture_url = db.Column(db.String(260))
-   # member_id = db.Column(db.Integer, db.ForeignKey('member.id', name='member_of_project'))
+    member_id = db.Column(db.Integer, db.ForeignKey('member.id', name='member_of_project'))
 
-# class Member(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(128))
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
